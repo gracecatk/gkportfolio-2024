8c316a5074670133f2483aec3d4b682baf800fe2
gracecatk <gracie.kiker@gmail.com>
2024-01-25 00:19:42 -0600

    everything added just needs personalization

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,26 @@
+export default function AboutMe() {
+    return (
+      <section id="AboutMe" className="about--section">
+        <div className="about--section--img">
+          <img src="./img/about-me.png" alt="" />
+        </div>
+        <div className="hero--section--content--box about--section--box">
+          <div className="hero--section--content">
+            <p className="section--title"></p>
+            <h1 className="skills-section--heading">About Me</h1>
+            <p className="hero--section-description">
+              I am a full-stack web developer with a passion for learning. I love my dog & my cat very much.
+              officiis sit debitis omnis harum sed veniam quasi dicta accusamus
+              recusandae? Voluptatem, reprehenderit alias? Eligendi aperiam
+              tempora numquam sint odit optio.
+            </p>
+            <p className="hero--section-description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+              officiis sit debitis omnis harum sed veniam quasi dicta accusamus
+              recusandae?
+            </p>
+          </div>
+        </div>
+      </section>
+    );
+  }
