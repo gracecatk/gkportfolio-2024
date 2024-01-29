74bbfc4da577d9cca4ba0dab905288377543a8d3
gracecatk <gracie.kiker@gmail.com>
2024-01-28 20:27:15 -0600

    final changes

--- src/Pages/Home/Navbar.jsx
+++ src/Pages/Home/Navbar.jsx
@@ -35,7 +35,7 @@ function Navbar() {
   return (
     <nav className={`navbar ${navActive ? "active" : ""}`}>
       <div>
-        <img src="./img/logo.svg" alt="Grace Kiker" />
+        <img src="" alt="Grace Kiker" />
       </div>
       <a
         className={`nav__hamburger ${navActive ? "active" : ""}`}
